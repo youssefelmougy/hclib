ffd710989253f3be46699ba0c3a85b9fe2ef0f79
sriraj <srirajpaul@gmail.com>
2017-11-24 06:52:01 +0000

    Add reference counting async_await

--- modules/ref-count/inc/hclib_ref-count.h
+++ modules/ref-count/inc/hclib_ref-count.h
@@ -47,6 +47,7 @@ class promise_t<T*>: public hclib::promise_t<T*> {
     }
 
     void ref_count_decr() {
+        if(ref_count == NULL) return;
         int cnt = __sync_sub_and_fetch(ref_count, 1);
 	HASSERT(cnt >= 0);
 	if(cnt == 0) {
@@ -65,6 +66,58 @@ template<typename T>
 void future_t<T*>::ref_count_decr() {
     promise_t<T*> * p = static_cast<promise_t<T*>*>(hclib_future_t::owner);
     p->ref_count_decr();
+}
+
+/*
+Async
+*/
+
+template <typename T>
+class AsyncRefCount {
+    T _lambda;
+    hclib_future_t *future1, *future2, *future3, *future4;
+
+  public:
+    AsyncRefCount(T lambda, hclib_future_t *f1, hclib_future_t *f2,
+        hclib_future_t *f3, hclib_future_t *f4)
+        : _lambda(lambda), future1(f1), future2(f2)
+        , future3(f3), future4(f4) {}
+
+    void operator() () {
+        _lambda();
+	future_t<void*> *f = static_cast<future_t<void*>*>(future1);
+	if(f != NULL)
+	    f->release();
+        f = static_cast<future_t<void*>*>(future2);
+	if(f != NULL)
+	    f->release();
+        f = static_cast<future_t<void*>*>(future3);
+	if(f != NULL)
+	    f->release();
+        f = static_cast<future_t<void*>*>(future4);
+	if(f != NULL)
+	    f->release();
+    }
+};
+
+template <typename T>
+inline void async_await(T&& lambda, hclib_future_t *future) {
+    AsyncRefCount<T> async_ref_count(lambda, future, NULL, NULL, NULL);
+    hclib::async_await(async_ref_count, future);
+}
+
+template <typename T>
+inline void async_await(T&& lambda, hclib_future_t *future1,
+        hclib_future_t *future2) {
+    AsyncRefCount<T> async_ref_count(lambda, future1, future2, NULL, NULL);
+    hclib::async_await(async_ref_count, future1, future2);
+}
+
+template <typename T>
+inline void async_await(T&& lambda, hclib_future_t *future1,
+        hclib_future_t *future2, hclib_future_t *future3, hclib_future_t *future4) {
+    AsyncRefCount<T> async_ref_count(lambda, future1, future2, future3, future4);
+    hclib::async_await(async_ref_count, future1, future2, future3, future4);
 }
 
 } // namespace ref_count
---
+++ modules/ref-count/test/ref-count0.cpp
@@ -0,0 +1,29 @@
+
+#include "hclib_cpp.h"
+#include "hclib_ref-count.h"
+#include <unistd.h>
+
+int main(int argc, char ** argv) {
+    int SIGNAL_VALUE = 42;
+    const char *deps[] = { "system"};
+    hclib::launch(deps, 1, [=]() {
+        hclib::finish([=]() {
+            hclib::ref_count::promise_t<int*> *prom = new hclib::ref_count::promise_t<int*>(1);
+
+            hclib::ref_count::async_await([=]() {
+                    int* signal = prom->get_future()->get();
+                    assert(*signal == SIGNAL_VALUE);
+                    printf("Value %d\n", *signal);
+                }, prom->get_future());
+            hclib::async([=]() {
+                    sleep(1);
+                    int *n= (int*) malloc(sizeof(int));
+                    *n = SIGNAL_VALUE;
+                    prom->put(n);
+                });
+        });
+    });
+    printf("Exiting...\n");
+    return 0;
+}
+
