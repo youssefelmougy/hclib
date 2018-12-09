309a391039a72ef999a08fddaa80f20619a2f3fb
sriraj <srirajpaul@gmail.com>
2018-12-09 13:08:43 -0800

    Add conditional compilation for OpenSHMEM and CraySHMEM

--- modules/node.js_openshmem/src/hclib_node.js_openshmem.cpp
+++ modules/node.js_openshmem/src/hclib_node.js_openshmem.cpp
@@ -11,17 +11,27 @@ HCLIB_MODULE_INITIALIZATION_FUNC(nodejs_openshmem_pre_initialize) {
     HASSERT(nic_locale_id > -1);
 }
 
+int shmem_init_thread(int, int*);
+
 HCLIB_MODULE_INITIALIZATION_FUNC(nodejs_openshmem_post_initialize) {
+
+#ifdef USE_CRAY_SHMEM_7
+    int ret = ::shmem_init_thread(SHMEM_THREAD_MULTIPLE);
+    assert(ret == SHMEM_THREAD_MULTIPLE);
+#else
     int major, minor;
     shmem_info_get_version(&major, &minor);
     if(major>=1 && minor>=4) {
-        int ret = ::shmem_init_thread(SHMEM_THREAD_MULTIPLE);
-        assert(ret == SHMEM_THREAD_MULTIPLE);
+        int provided;
+        int ret = ::shmem_init_thread(SHMEM_THREAD_MULTIPLE, &provided);
+        assert(ret == 0);
+        assert(provided == SHMEM_THREAD_MULTIPLE);
     }
     else {
-        //::shmem_init();
-        ::shmem_init_thread(SHMEM_THREAD_MULTIPLE);
+        printf("WARNING: SHMEM 1.4 or above required\n");
+        ::shmem_init();
     }
+#endif
 
     int n_nics;
     hclib::locale_t **nics = hclib::get_all_locales_of_type(nic_locale_id,
