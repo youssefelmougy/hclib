8acd92375ee19433406ce4692b52338e148ac716
JKChenFZ <kunfz96@gmail.com>
2020-03-05 23:52:57 -0500

    modified test selector with yield usage

--- modules/bale_actor/test/test_selector3.cpp
+++ modules/bale_actor/test/test_selector3.cpp
@@ -9,6 +9,7 @@
 #include "selector.h"
 
 class TestSelector: public hclib::Selector<3, int64_t> {
+    int64_t* counterPtr_;
 
     void process0(int64_t pkt, int sender_rank) {
         printf("Process0 In rank %d, val %d, from rank %d\n", shmem_my_pe(), pkt, sender_rank);
@@ -22,11 +23,12 @@ class TestSelector: public hclib::Selector<3, int64_t> {
 
     void process2(int64_t pkt, int sender_rank) {
         printf("Process2 In rank %d, val %d, from rank %d\n", shmem_my_pe(), pkt, sender_rank);
+        (*counterPtr_)++;
     }
 
   public:
 
-    TestSelector() {
+    TestSelector(int64_t* counterPtr) : counterPtr_(counterPtr) {
         mb[0].process = [this](int64_t pkt, int sender_rank) { this->process0(pkt, sender_rank); };
         mb[1].process = [this](int64_t pkt, int sender_rank) { this->process1(pkt, sender_rank); };
         mb[2].process = [this](int64_t pkt, int sender_rank) { this->process2(pkt, sender_rank); };
@@ -37,17 +39,28 @@ int main() {
 
   const char *deps[] = { "system", "bale_actor" };
   hclib::launch(deps, 2, [=] {
+    int64_t counter = 0;
+    int64_t* counterPtr = &counter;
 
-    TestSelector *ts_ptr = new TestSelector();
+    TestSelector *ts_ptr = new TestSelector(counterPtr);
 
     printf("Finished inititialization\n");
     hclib::finish([=]() {
     hclib::selector::finish(ts_ptr, [=]() { //finish will start the ts selector and wait for ts to finish all communication
-      int num = 10, dest_rank = (shmem_my_pe() + 1)%shmem_n_pes();
-      for(int i=0;i<num;i++) {
-          int64_t val = shmem_my_pe() * 1000 + i;
-          ts_ptr->send(0, val, dest_rank);
+      while (*counterPtr < 10) {
+        printf("DEBUG: counterPtr has value %lu \n", *counterPtr);
+        int num = 10, dest_rank = (shmem_my_pe() + 1)%shmem_n_pes();
+        for(int i=0;i<num;i++) {
+            int64_t val = shmem_my_pe() * 1000 + i;
+            // ts_ptr->send(0, val, dest_rank);
+            ts_ptr->send(2, val, dest_rank);
+        }
+
+        hclib::yield();
+        printf("DEBUG: after yield counterPtr has value %lu \n", *counterPtr);
       }
+      printf("DEBUG: ready to teardown\n");
+
       ts_ptr->done(0); // Indicate that we are done with sending messages to the REQUEST mailbox
     });
     });
