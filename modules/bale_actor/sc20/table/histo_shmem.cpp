f6983c14ee7f4328605ced8744bd43976139d7eb
sriraj <srirajpaul@gmail.com>
2020-05-27 23:51:40 -0700

    Add sc20 table programs with shmem and conveyor versions

---
+++ modules/bale_actor/sc20/table/histo_shmem.cpp
@@ -0,0 +1,221 @@
+
+/******************************************************************
+//
+//
+//  Copyright(C) 2019, Institute for Defense Analyses
+//  4850 Mark Center Drive, Alexandria, VA; 703-845-2500
+//  This material may be reproduced by or for the US Government
+//  pursuant to the copyright license under the clauses at DFARS
+//  252.227-7013 and 252.227-7014.
+//
+//
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//    * Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//    * Neither the name of the copyright holder nor the
+//      names of its contributors may be used to endorse or promote products
+//      derived from this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+//  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+//  COPYRIGHT HOLDER NOR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+//  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+//  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+//  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+//  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+//  OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+ *****************************************************************/
+
+/*! \file histo_shmem.cpp
+ * \brief An implementation of histogram that uses OpenSHMEM global atomics.
+ */
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <shmem.h>
+
+#define THREADS shmem_n_pes()
+#define MYTHREAD shmem_my_pe()
+
+#include <time.h>
+#include <sys/time.h>
+
+#define T0_fprintf if(MYTHREAD==0) fprintf
+
+double wall_seconds() {
+  struct timeval tp;
+  int retVal = gettimeofday(&tp,NULL);
+  if (retVal == -1) { perror("gettimeofday:"); fflush(stderr); }
+  return ( (double) tp.tv_sec + (double) tp.tv_usec * 1.e-6 );
+}
+
+static void *setup_shmem_reduce_workdata(long **psync, size_t xsize) {
+  void *work;
+  int i;
+
+  work=shmem_malloc(_SHMEM_REDUCE_MIN_WRKDATA_SIZE*xsize);
+  *psync=(long *)shmem_malloc(_SHMEM_REDUCE_SYNC_SIZE*sizeof(long));
+  for(i=0;i<_SHMEM_REDUCE_SYNC_SIZE;++i) {
+    (*psync)[i]=_SHMEM_SYNC_VALUE;
+  }
+  shmem_barrier_all();
+  return work;
+}
+
+double reduce_add(double myval){
+  static double *buff=NULL, *work;
+  static long *sync;
+  if (buff==NULL) {
+    buff=(double*)shmem_malloc(2*sizeof(double));
+    work=(double*)setup_shmem_reduce_workdata(&sync,sizeof(double));
+  }
+  buff[0]=myval;
+
+  shmem_double_sum_to_all(&buff[1],buff,1,0,0,shmem_n_pes(),work,sync);
+
+  shmem_barrier_all();
+  return buff[1];
+}
+
+/*!
+ * \brief This routine implements straight forward,
+ *         single word atomic updates to implement histogram.
+ * \param *index array of indices into the shared array of counts.
+ * \param l_num_ups the local length of the index array
+ * \param *counts SHARED pointer to the count array.
+ * \return average run time
+ *
+ */
+double histo_shmem(int64_t *pckindx, int64_t T, int64_t *counts) {
+  int ret;
+  int64_t i;
+  double tm;
+  int64_t pe, col;
+  double stat;
+
+  shmem_barrier_all();
+  tm = wall_seconds();
+  i = 0UL;
+  for(i = 0; i < T; i++) {
+    col = pckindx[i] >> 16;
+    pe  = pckindx[i] & 0xffff;
+    //shmem_int64_atomic_add(counts+col, 1, pe);
+    shmem_int64_atomic_inc(counts+col, pe);
+  }
+  shmem_barrier_all();
+  tm = wall_seconds() - tm;
+  stat = reduce_add(tm)/shmem_n_pes();
+  return stat;
+}
+
+int main(int argc, char * argv[]) {
+  shmem_init();
+
+  //char hostname[1024];
+  //hostname[1023] = '\0';
+  //gethostname(hostname, 1023);
+  //printf("Hostname: %s rank: %d\n", hostname, MYTHREAD);
+
+  int64_t l_num_ups  = 1000000;     // per thread number of requests (updates)
+  int64_t lnum_counts = 1000;       // per thread size of the table
+
+  int64_t i;
+
+  int printhelp = 0;
+  int opt;
+  while( (opt = getopt(argc, argv, "hn:T:")) != -1 ) {
+    switch(opt) {
+    case 'h': printhelp = 1; break;
+    case 'n': sscanf(optarg,"%ld" ,&l_num_ups);  break;
+    case 'T': sscanf(optarg,"%ld" ,&lnum_counts);  break;
+    default:  break;
+    }
+  }
+
+  T0_fprintf(stderr,"Running histo on %d PEs\n", THREADS);
+  T0_fprintf(stderr,"Number updates / PE              (-n)= %ld\n", l_num_ups);
+  T0_fprintf(stderr,"Table size / PE                  (-T)= %ld\n", lnum_counts);
+  fflush(stderr);
+
+
+  // Allocate and zero out the counts array
+  int64_t num_counts = lnum_counts*THREADS;
+  int64_t * counts = (int64_t *)shmem_malloc(lnum_counts* sizeof(int64_t)); assert(counts != NULL);
+  int64_t *lcounts = counts;
+  for(i = 0; i < lnum_counts; i++)
+    lcounts[i] = 0L;
+
+  // index is a local array of indices into the shared counts array.
+  // This is used by the * version.
+  // To avoid paying the UPC tax of computing index[i]/THREADS and index[i]%THREADS
+  // when using the exstack and conveyor models
+  // we also store a packed version that holds the pe (= index%THREADS) and lindx (=index/THREADS)
+  int64_t *index   = (int64_t *) calloc(l_num_ups, sizeof(int64_t)); assert(index != NULL);
+  int64_t *pckindx = (int64_t *) calloc(l_num_ups, sizeof(int64_t)); assert(pckindx != NULL);
+  int64_t indx, lindx, pe;
+
+  srand(MYTHREAD + 120348);
+  for(i = 0; i < l_num_ups; i++) {
+    //indx = i % num_counts;          //might want to do this for debugging
+    indx = rand() % num_counts;
+    index[i] = indx;
+    lindx = indx / THREADS;
+    pe  = indx % THREADS;
+    pckindx[i]  =  (lindx << 16L) | (pe & 0xffff);
+  }
+
+  shmem_barrier_all();
+
+  int64_t use_model;
+  double laptime = 0.0;
+  int64_t num_models = 0L;               // number of models that are executed
+
+      laptime = histo_shmem(pckindx, l_num_ups, counts);
+      num_models++;
+
+      T0_fprintf(stderr,"  %8.3lf seconds\n", laptime);
+
+  shmem_barrier_all();
+
+  // Check the results
+  // Assume that the atomic add version will correctly zero out the counts array
+  for(i = 0; i < l_num_ups; i++) {
+    lindx = index[i] / THREADS;
+    pe  = index[i] % THREADS;
+    shmem_int64_atomic_add(counts+lindx, -num_models, pe); 
+  }
+  shmem_barrier_all();
+
+  int64_t num_errors = 0, totalerrors = 0;
+  for(i = 0; i < lnum_counts; i++) {
+    if(lcounts[i] != 0L) {
+      num_errors++;
+      if(num_errors < 5)  // print first five errors, report number of errors below
+        fprintf(stderr,"ERROR: Thread %d error at %ld (= %ld)\n", MYTHREAD, i, lcounts[i]);
+    }
+  }
+  totalerrors = (double)reduce_add((double)num_errors);
+  if(totalerrors) {
+     T0_fprintf(stderr,"FAILED!!!! total errors = %ld\n", totalerrors);
+  }
+
+  shmem_free(counts);
+  free(index);
+  free(pckindx);
+  shmem_finalize();
+
+  return 0;
+}
+
---
+++ modules/bale_actor/sc20/table/ig_shmem.cpp
@@ -0,0 +1,216 @@
+/******************************************************************
+//
+//
+//  Copyright(C) 2019, Institute for Defense Analyses
+//  4850 Mark Center Drive, Alexandria, VA; 703-845-2500
+//  This material may be reproduced by or for the US Government
+//  pursuant to the copyright license under the clauses at DFARS
+//  252.227-7013 and 252.227-7014.
+// 
+//
+//  All rights reserved.
+//  
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//    * Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//    * Neither the name of the copyright holder nor the
+//      names of its contributors may be used to endorse or promote products
+//      derived from this software without specific prior written permission.
+// 
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+//  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+//  COPYRIGHT HOLDER NOR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+//  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+//  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+//  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+//  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+//  OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+ *****************************************************************/ 
+
+/*! \file ig_shmem.cpp
+ * \brief An implementation of indexgather that uses single word gets to shared addresses.
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <shmem.h>
+
+#define THREADS shmem_n_pes()
+#define MYTHREAD shmem_my_pe()
+
+#include <time.h>
+#include <sys/time.h>
+
+#define T0_fprintf if(MYTHREAD==0) fprintf
+
+double wall_seconds() {
+  struct timeval tp;
+  int retVal = gettimeofday(&tp,NULL);
+  if (retVal == -1) { perror("gettimeofday:"); fflush(stderr); }
+  return ( (double) tp.tv_sec + (double) tp.tv_usec * 1.e-6 );
+}
+
+static void *setup_shmem_reduce_workdata(long **psync, size_t xsize) {
+  void *work;
+  int i;
+
+  work=shmem_malloc(_SHMEM_REDUCE_MIN_WRKDATA_SIZE*xsize);
+  *psync=(long *)shmem_malloc(_SHMEM_REDUCE_SYNC_SIZE*sizeof(long));
+  for(i=0;i<_SHMEM_REDUCE_SYNC_SIZE;++i) {
+    (*psync)[i]=_SHMEM_SYNC_VALUE;
+  }
+  shmem_barrier_all();
+  return work;
+}
+
+double reduce_add(double myval){
+  static double *buff=NULL, *work;
+  static long *sync;
+  if (buff==NULL) {
+    buff=(double*)shmem_malloc(2*sizeof(double));
+    work=(double*)setup_shmem_reduce_workdata(&sync,sizeof(double));
+  }
+  buff[0]=myval;
+
+  shmem_double_sum_to_all(&buff[1],buff,1,0,0,shmem_n_pes(),work,sync);
+
+  shmem_barrier_all();
+  return buff[1];
+}
+
+/*!
+ * \brief This routine implements the single word get version indexgather
+ * \param *tgt array of target locations for the gathered values
+ * \param *index array of indices into the global array of counts
+ * \param l_num_req the length of the index array
+ * \param *table shared pointer to the shared table array.
+ * \return average run time
+ *
+ */
+double ig_shmem(int64_t *tgt, int64_t *pckindx, int64_t l_num_req,  int64_t *table) {
+  int64_t i;
+  int64_t pe, col;
+  double tm;
+  double stat;
+
+  shmem_barrier_all();
+  tm = wall_seconds();
+
+  for(i = 0; i < l_num_req; i++){
+    col = pckindx[i] >> 16;
+    pe  = pckindx[i] & 0xffff;
+    tgt[i] = shmem_int64_g(table+col, pe);
+  }
+
+  tm = wall_seconds() - tm;
+  shmem_barrier_all();
+
+  stat = reduce_add(tm)/shmem_n_pes();
+
+  return stat;
+}
+
+int64_t ig_check_and_zero(int64_t use_model, int64_t *tgt, int64_t *index, int64_t l_num_req) {
+  int64_t errors=0;
+  int64_t i;
+  shmem_barrier_all();
+  for(i=0; i<l_num_req; i++){
+    if(tgt[i] != (-1)*(index[i] + 1)){
+      errors++;
+      if(errors < 5)  // print first five errors, report all the errors
+        fprintf(stderr,"ERROR: model %ld: Thread %d: tgt[%ld] = %ld != %ld)\n",
+                use_model,  MYTHREAD, i, tgt[i], (-1)*(index[i] + 1));
+               //use_model,  MYTHREAD, i, tgt[i],(-1)*(i*THREADS+MYTHREAD + 1) );
+    }
+    tgt[i] = 0;
+  }
+  if( errors > 0 )
+    fprintf(stderr,"ERROR: %ld: %ld total errors on thread %d\n", use_model, errors, MYTHREAD);
+  shmem_barrier_all();
+  return(errors);
+}
+
+int main(int argc, char * argv[]) {
+  shmem_init();
+  
+  int64_t i;
+  int64_t ltab_siz = 100000;
+  int64_t l_num_req  = 1000000;      // number of requests per thread
+  int64_t num_errors = 0L, total_errors = 0L;
+  int64_t printhelp = 0;
+
+  int opt; 
+  while( (opt = getopt(argc, argv, "hn:T:")) != -1 ) {
+    switch(opt) {
+    case 'h': printhelp = 1; break;
+    case 'n': sscanf(optarg,"%ld" ,&l_num_req);   break;
+    case 'T': sscanf(optarg,"%ld" ,&ltab_siz);   break;
+    default:  break;
+    }
+  }
+
+  T0_fprintf(stderr,"Running ig on %d PEs\n", THREADS);
+  T0_fprintf(stderr,"Number of Request / PE           (-n)= %ld\n", l_num_req );
+  T0_fprintf(stderr,"Table size / PE                  (-T)= %ld\n", ltab_siz);
+  
+  // Allocate and populate the shared table array 
+  int64_t tab_siz = ltab_siz*THREADS;
+  int64_t * table  = (int64_t*)shmem_malloc(ltab_siz* sizeof(int64_t)); assert(table != NULL);
+  int64_t *ltable  = table;
+  // fill the table with the negative of its shared index
+  // so that checking is easy
+  for(i=0; i<ltab_siz; i++)
+    ltable[i] = (-1)*(i*THREADS + MYTHREAD + 1);
+  
+  // As in the histo example, index is used by the * version.
+  // pckindx is used my the buffered versions
+  int64_t *index   =  (int64_t*)calloc(l_num_req, sizeof(int64_t)); assert(index != NULL);
+  int64_t *pckindx =  (int64_t*)calloc(l_num_req, sizeof(int64_t)); assert(pckindx != NULL);
+  int64_t indx, lindx, pe;
+  srand(MYTHREAD+ 5 );
+  for(i = 0; i < l_num_req; i++){
+    indx = rand() % tab_siz;
+    index[i] = indx;
+    lindx = indx / THREADS;      // the distributed version of indx
+    pe  = indx % THREADS;      
+    pckindx[i] = (lindx << 16) | (pe & 0xffff); // same thing stored as (local index, thread) "shmem style"
+  }
+
+  int64_t *tgt  =  (int64_t*)calloc(l_num_req, sizeof(int64_t)); assert(tgt != NULL);
+
+  shmem_barrier_all();
+
+  int64_t use_model;
+  double laptime = 0.0;
+
+        laptime = ig_shmem(tgt, pckindx, l_num_req,  ltable);
+
+     T0_fprintf(stderr,"  %8.3lf seconds\n", laptime);
+   
+     num_errors += ig_check_and_zero(use_model, tgt, index, l_num_req);
+
+  total_errors = num_errors;
+  if( total_errors ) {
+    T0_fprintf(stderr,"YOU FAILED!!!!\n");
+  } 
+
+  shmem_barrier_all();
+  shmem_free(table);
+  free(index);
+  free(pckindx);
+  free(tgt);
+  shmem_finalize();
+  return 0;
+}
+
