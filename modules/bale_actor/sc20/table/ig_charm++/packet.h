f47aea757428ec57583dd7eebb87a950d3cf4094
Akihiro Hayashi <ahayashi@rice.edu>
2020-06-04 09:26:09 -0700

    Add Charm++ version of histo and ig

---
+++ modules/bale_actor/sc20/table/ig_charm++/packet.h
@@ -0,0 +1,27 @@
+#ifndef _PACKET_H_
+#define _PACKET_H_
+class packet1 {
+ public:
+  CmiInt8 val;
+  CmiInt8 idx;
+  CmiInt8 pe;
+
+  void pup(PUP::er &p) {
+    // remember to pup your superclass if there is one
+    p|val;
+    p|idx;
+    p|pe;
+  }
+};
+class packet2 {
+ public:
+  CmiInt8 val;
+  CmiInt8 idx;
+
+  void pup(PUP::er &p) {
+    // remember to pup your superclass if there is one
+    p|val;
+    p|idx;
+  }
+};
+#endif
