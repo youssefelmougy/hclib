279e3dc4ae03bf61b4d00f8c749d4e49bb86d837
Akihiro Hayashi <ahayashi@gatech.edu>
2021-08-12 19:38:05 +0000

    Add non get_selector version of ig, permute, randperm.

--- modules/bale_actor/test/trans.sh
+++ modules/bale_actor/test/trans.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -x
 
-$LLVM_HOME/bin/selector-trans $1_selector_lambda.cpp -- -I/root/local/include -I$BALE_INSTALL/include -I$HCLIB_ROOT/include -I$PWD/../inc | sed 's/#define USE_LAMBDA/#undef USE_LAMBDA/' | $LLVM_HOME/bin/clang-format > $1_selector_lambda.trans.cpp
+$LLVM_HOME/bin/selector-trans $1_selector_lambda$2.cpp -- -I/root/local/include -I$BALE_INSTALL/include -I$HCLIB_ROOT/include -I$PWD/../inc | sed 's/#define USE_LAMBDA/#undef USE_LAMBDA/' | $LLVM_HOME/bin/clang-format > $1_selector_lambda$2.trans.cpp
--- modules/bale_actor/test/trans_all.sh
+++ modules/bale_actor/test/trans_all.sh
@@ -5,3 +5,12 @@ do
     ./trans.sh ${app}
     make ${app}_selector_lambda.trans
 done
+
+./trans.sh ig 3
+make ig_selector_lambda3.trans
+
+./trans.sh permute 2
+make permute_selector_lambda2.trans
+
+./trans.sh randperm 2
+make randperm_selector_lambda2.trans
